750ef78a36a4ffb7a1f078d8ce25c01886a237db
Ravi Bharti <ravics1718@gmail.com>
2021-09-05 00:47:18 +0530

    first commit 🚀

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className=" font-bold text-3xl text-blue-800">
+     Welcome to the app🌍
+    </div>
+  )
+}
+
+export default App
